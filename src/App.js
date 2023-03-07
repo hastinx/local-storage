1928eb963b17a4ddb2644d2e24a2fef74f1bae0c
MuhammadHastin <hastinre@gmail.com>
2023-03-07 20:59:54 +0700

    push11

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import List from './component/list';
 import Input from './component/input';
 
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route exact path='/ls/' element={<List />} />
         <Route path='/ls/input' element={<Input />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
